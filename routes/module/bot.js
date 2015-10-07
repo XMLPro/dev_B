602eb1003734fe5048b428a92adcf7d34d97da5b
matsuneko22 <dango.umai@outlook.jp>
2015-10-07 09:17:58 +0900

    2

--- routes/module/bot.js
+++ routes/module/bot.js
@@ -1,7 +1,13 @@
 console.log("module load");
 var bot = {};
+var MeCab = new require('mecab-async')
+      , mecab = new MeCab();
 
+      
+//特定のキーワードを判別する関数。
+//該当しなければexception投げます。キャッチしてあげください。
 bot.reply = function(msg){
+  bot.MAnalysis(msg);
   if(msg === "こんにちは"){
     return "こんにちは";
   }else if(msg === "こんばんわ"){
@@ -11,5 +17,12 @@ bot.reply = function(msg){
   }
 };
 
+//形態素解析する関数。引数で解析したい文字列を引数として渡すとコンソールで出力。
+bot.MAnalysis = function(str){
+  mecab.parse(str, function(err, result) {
+      if (err) throw err;
+      console.log(result);
+  });
+}
 
 module.exports = bot;
